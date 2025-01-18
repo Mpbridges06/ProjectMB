228d66939d9d826afb6507c3e6daf72a8acdad38
Mpbridges06 <bridges_miles@students.columbusstate.edu>
2025-01-18 16:09:21 -0500

    Add files via upload

---
+++ First.java
@@ -0,0 +1,11 @@
+//Miles Bridges Jan 17, 2024
+package snippet;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Information Security Analyst");
+        System.out.println("Why: Growing demand for cybersecurity professionals");
+        System.out.println("Median salary: $120,360");
+    }
+}
